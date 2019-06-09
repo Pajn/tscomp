edc39a0e9136a18f088909ebee13590dc33eee31
Rasmus Eneman <rasmus@eneman.eu>
2019-06-09 22:50:06 +0200

    fix: Correctly read .env files and run preflight checks in watch mode

--- packages/tscomp-scripts/scripts/watch.js
+++ packages/tscomp-scripts/scripts/watch.js
@@ -19,11 +19,15 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
-// Load environment variables from .env file. Suppress warnings using silent
-// if this file is missing. dotenv will never modify any environment variables
-// that have already been set.
-// https://github.com/motdotla/dotenv
-require('dotenv').config({ silent: true });
+// Ensure environment variables are read.
+require('../config/env');
+// @remove-on-eject-begin
+// Do the preflight checks (only happens before eject).
+const verifyPackageTree = require('./utils/verifyPackageTree');
+if (process.env.SKIP_PREFLIGHT_CHECK !== 'true') {
+  verifyPackageTree();
+}
+// @remove-on-eject-end
 
 const chalk = require('react-dev-utils/chalk');
 const gulp = require('../config/gulp');
